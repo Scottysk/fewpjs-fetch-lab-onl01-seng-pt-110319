8b2915e9b38770d6a9344ff114abe906ad43b3c9
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-06-06 22:56:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function fetchBooks() {
   return fetch("https://anapioficeandfire.com/api/books")
   .then(resp => resp.json())
-  .then(json => renderBooks(books))
+  .then(json => renderBooks(json))
 }
 
 function renderBooks(books) {
